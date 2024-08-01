c21f38786fc4ca54954d24f5b7d3d6be1dc8498d
TieuHoangDat <DatTH.B21CN220@stu.ptit.edu.vn>
2024-08-01 12:02:37 +0700

    guard, refresh token

--- src/redux/authSlice/apiCalls.js
+++ src/redux/authSlice/apiCalls.js
@@ -11,14 +11,13 @@ export const login = async (user, dispatch) => {
 		const url = apiUrl;
 		const { data } = await axios.post(url, user);
 		console.log(data)
-		const decodeData = jwt_decode(data.data);
-		// if (decodeData.role!==1) {
-		// 	toast.error("You don't have access");
-		// 	dispatch(loginFailure());
-		// 	return;
-		// }
+		const decodeData = jwt_decode(data.access_token);
+
+		localStorage.setItem('access_token', data.access_token);
+    	localStorage.setItem('refresh_token', data.refresh_token);
+
 		toast.success(data.message);
-		dispatch(loginSuccess({ ...decodeData, token: data.data }));
+		dispatch(loginSuccess({ ...decodeData, token: data.access_token }));
 		window.location = "/";
 	} catch (error) {
 		dispatch(loginFailure());
--- src/redux/axiosInstance.js
+++ src/redux/axiosInstance.js
@@ -1,40 +1,60 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-let token = null;
-const root = JSON.parse(window.localStorage.getItem("persist:root"));
+// let token = null;
+// const root = JSON.parse(window.localStorage.getItem("persist:root"));
 
-if (root) {
-	const { auth } = root;
-	const { user } = JSON.parse(auth);
-	if (user) token = user.token;
-}
+// if (root) {
+// 	const { auth } = root;
+// 	const { user } = JSON.parse(auth);
+// 	if (user) token = user.token;
+// }
+
+let token = localStorage.getItem('access_token');
 
 const axiosInstance = axios.create({
 	baseURL: process.env.REACT_APP_API_URL,
 	headers: {
-		"Content-Type": "application/json",
-		"x-auth-token": token ? token : "",
+		'Content-Type': 'application/json',
+		Authorization: token ? `Bearer ${token}` : '',
 	},
 });
 
 axiosInstance.interceptors.response.use(
-	(req) => {
-		return Promise.resolve(req);
+	(response) => {
+	  return response;
 	},
-	(error) => {
-		if (
-			error.response &&
-			error.response.status >= 400 &&
-			error.response.status < 500
-		) {
-			toast.error(error.response.data);
-		} else {
-			console.log(error);
-			toast.error("Something went wrong!");
+	async (error) => {
+		console.log("ok")
+	  const originalRequest = error.config;
+	  if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+		try {
+			console.log("ok1") //
+		  const newAccessToken = await refreshAccessToken();
+		  axios.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`;
+		  originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+		  return axiosInstance(originalRequest);
+		} catch (refreshError) {
+		  console.error('Unable to refresh token', refreshError);
+		  toast.error('Session expired. Please log in again.');
+		  window.location.href = '/login'; // Redirect to login page
+		  return Promise.reject(refreshError);
 		}
-		return Promise.reject(error);
+	  }
+	  return Promise.reject(error);
 	}
 );
+
+const refreshAccessToken = async () => {
+	const refresh_token = localStorage.getItem('refresh_token');
+	if (!refresh_token) {
+		throw new Error('No refresh token available');
+	}
+	const response = await axiosInstance.post('/auth/refresh', { refresh_token });
+	const { access_token } = response.data;
+	console.log(access_token) //
+	localStorage.setItem('access_token', access_token);
+	return access_token;
+};
 
 export default axiosInstance;
